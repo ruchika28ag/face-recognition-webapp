342f73bea508e4e2217223e24c2f76c3e58d6a5d
Ruchika Agrawal <rockstarruchi28@gmail.com>
2019-09-18 00:43:31 +0530

    bugs fix

--- facerecognition api/server.js
+++ facerecognition api/server.js
@@ -49,7 +49,7 @@ const app= express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res)=>{	res.json('it is working')	})
+app.get('/', (req, res)=>{	res.send('it is working')	})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', register.handleRegister(db, bcrypt))
 app.get('/profile/:id', profile.handleProfileGet(db))
