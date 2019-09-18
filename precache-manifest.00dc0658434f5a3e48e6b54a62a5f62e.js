21c9e5c7c1867fa8fae5063adc393076c4d67c6f
Ruchika Agrawal <rockstarruchi28@gmail.com>
2019-09-18 18:44:35 +0530

    Updates

---
+++ precache-manifest.00dc0658434f5a3e48e6b54a62a5f62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e14c661b8b546a4e7fccd9e6a614e3",
+    "url": "/face-recognition-webapp/index.html"
+  },
+  {
+    "revision": "7d5c6511e881af8e0584",
+    "url": "/face-recognition-webapp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "51a124f3a29c6361836a",
+    "url": "/face-recognition-webapp/static/css/main.a2da6bfa.chunk.css"
+  },
+  {
+    "revision": "7d5c6511e881af8e0584",
+    "url": "/face-recognition-webapp/static/js/2.67d7a9b0.chunk.js"
+  },
+  {
+    "revision": "51a124f3a29c6361836a",
+    "url": "/face-recognition-webapp/static/js/main.03867a32.chunk.js"
+  },
+  {
+    "revision": "7379200cda0c14124727",
+    "url": "/face-recognition-webapp/static/js/runtime~main.d545e528.js"
+  }
+]);
