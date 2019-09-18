9d19d43a3524c04b84cb9c0e7586e5f0fb35bc29
Ruchika Agrawal <rockstarruchi28@gmail.com>
2019-09-18 18:37:31 +0530

    added docs folder

---
+++ docs/precache-manifest.6179eb4bdaa6932ae2ecd4c2323b64ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a56acf3abff2547c00b6c010c22637a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d5c6511e881af8e0584",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "20df2cb5547b36d785c7",
+    "url": "/static/css/main.a2da6bfa.chunk.css"
+  },
+  {
+    "revision": "7d5c6511e881af8e0584",
+    "url": "/static/js/2.67d7a9b0.chunk.js"
+  },
+  {
+    "revision": "20df2cb5547b36d785c7",
+    "url": "/static/js/main.ba794fd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
